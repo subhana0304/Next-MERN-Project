59e2d7a128cc5cb1a5cc1dfbf7911421a334e725
subhana0304 <subhana5721194@gmail.com>
2025-01-07 18:49:37 +0600

    changed new name

--- src/app/CreateProject/page.js
+++ src/app/CreateProject/page.js
@@ -43,7 +43,7 @@ export default function CreateProject() {
     });
   
     try {
-      const response = await fetch('https://next-mern-project.vercel.app/api/projects', {
+      const response = await fetch('https://next-mern-project-604r16s9q-subhana0304s-projects.vercel.app/api/projects', {
         method: 'POST',
         headers: {
           "Content-type": "application/json",
