d1e679c7ff14e2578c452ef41a66d0b61c447666
subhana0304 <subhana5721194@gmail.com>
2025-01-07 22:47:56 +0600

    did main url

--- src/app/CreateProject/page.js
+++ src/app/CreateProject/page.js
@@ -43,7 +43,7 @@ export default function CreateProject() {
     });
   
     try {
-      const response = await fetch(`${process.env.WEBSITE_URL}/api/projects`, {
+      const response = await fetch('https://next-mern-project.netlify.app/api/projects', {
         method: 'POST',
         headers: {
           "Content-type": "application/json",
